d0f1681bdbf27591cd70b06437eab78f838516c8
hisen <test@hisen.com>
2016-05-03 19:53:45 +0800

    d

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hisen_shi on 2016/5/3.
- */
-public class HelloWorld {
-    public static void main(String []args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Git");
-    }
-}
